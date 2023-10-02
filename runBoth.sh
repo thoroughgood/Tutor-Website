9cb4843787ef20f7d5d857a03bb59acf65a1c51a
ddannyll <daniel.nguyen.s173@gmail.com>
2023-10-02 21:46:52 +1100

    dx, fix: CRITICAL FIX added trap to kill child processes in run both

--- runBoth.sh
+++ runBoth.sh
@@ -3,8 +3,14 @@
 cd backend
 
 ./runBackend.sh &
+pid1=$!
 
 cd ../frontend
 
 npm i
-npm run dev
+npm run dev &
+pid2=$!
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+trap "echo finished running both" SIGINT
+wait "$pid1" "$pid2" 
