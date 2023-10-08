d56f2967b93073a114028324d2662307495c3d2d
ddannyll <daniel.nguyen.s173@gmail.com>
2023-10-09 00:58:33 +1100

    feat: find all tutors

---
+++ frontend/src/pages/findTutor.tsx
@@ -0,0 +1,9 @@
+import SmallProfileCard from "@/components/smallProfileCard"
+
+export default function FindTutor() {
+  return (
+    <div className="h-full w-full p-16">
+      <SmallProfileCard id="1" accountType="tutor" />
+    </div>
+  )
+}
