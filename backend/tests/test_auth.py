6a8fbc6d5651c352536122b366f25d21117f9e12
Victor Tang <ultrodude@gmail.com>
2023-10-08 23:29:48 +1100

    Some tests for login and logout

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 import pytest
 from flask.testing import FlaskClient
 from prisma.models import Tutor, Student
@@ -115,3 +116,172 @@ def test_register_args(setup_test: FlaskClient):
 
     assert Student.prisma().count() == 1
     assert Tutor.prisma().count() == 1
+
+################################# LOGIN TESTS ##################################
+
+def test_login_not_json(setup_test: FlaskClient):
+    client = setup_test
+    resp = client.post("/login")
+    assert resp.json == {"error": "content-type was not json or data was malformed"}
+    assert resp.status_code == 415
+
+@pytest.fixture
+def initialise_student() -> str:
+    student = Student.prisma().create(
+        data={
+            "id": str(uuid4()),
+            "email": "validemail@mail.com",
+            "hashedPassword": "12345678",
+            "name": "Name1",
+            "bio": "",
+            "location": "Australia",
+        },
+    )
+    return student.id
+
+@pytest.fixture
+def initialise_tutor() -> str:
+    tutor = Tutor.prisma().create(
+        data={
+            "id": str(uuid4()),
+            "email": "validemail2@mail.com",
+            "hashedPassword": "12345678",
+            "name": "Name2",
+            "bio": "",
+            "location": "Australia",
+        },
+    )
+    return tutor.id
+
+def test_login_args(setup_test: FlaskClient, initialise_student: str):
+    client = setup_test
+    # Missing email
+    resp = client.post("/login", json={})
+    assert resp.json == {"error": "email field was missing"}
+    assert resp.status_code == 400
+
+    # Invalid Email
+    resp = client.post("/login", json={"email": "notvalidemail"})
+    assert resp.json == {"error": "email field is invalid"}
+    assert resp.status_code == 400
+
+    # Missing password
+    resp = client.post("/login", json={"email": "validemail@mail.com"})
+    assert resp.json == {"error": "password field must be at least 8 characters long"}
+    assert resp.status_code == 400
+
+    # Invalid password
+    resp = client.post(
+        "/login", json={"email": "validemail@mail.com", "password": "ab12"}
+    )
+    assert resp.json == {"error": "password field must be at least 8 characters long"}
+    assert resp.status_code == 400
+
+    # Missing accountType
+    resp = client.post(
+        "/login",
+        json={ "email": "validemail@mail.com", "password": "12345678"},
+    )
+    assert resp.json == {"error": "accountType must be 'student' or 'tutor'"}
+    assert resp.status_code == 400
+
+    # Invalid accountType
+    resp = client.post(
+        "/login",
+        json={
+            "email": "validemail@mail.com",
+            "password": "12345678",
+            "accountType": "notvalid",
+        },
+    )
+    assert resp.json == {"error": "accountType must be 'student' or 'tutor'"}
+    assert resp.status_code == 400
+
+    # Invalid login attempt (wrong password)
+    resp = client.post(
+        "/login",
+        json={
+            "email": "validemail@mail.com",
+            "password": "12345677",
+            "accountType": "student",
+        },
+    )
+    assert resp.json == {"error": "Invalid login attempt"}
+    assert resp.status_code == 401
+
+    # Successful login attempt (student)
+    resp = client.post(
+        "/login",
+        json={
+            "email": "validemail@mail.com",
+            "password": "12345678",
+            "accountType": "student",
+        },
+    )
+    assert resp.status_code == 200
+    assert resp.json["success"] == True
+    with client.session_transaction() as session:
+        assert session["user_id"] == initialise_student
+
+# Successful login attempt (student)
+def test_tutor_login(setup_test: FlaskClient, initialise_tutor: str):
+    client = setup_test
+    resp = client.post(
+        "/login",
+        json={
+            "email": "validemail2@mail.com",
+            "password": "12345678",
+            "accountType": "tutor",
+        },
+    )
+    assert resp.status_code == 200
+    assert resp.json["success"] == True
+    with client.session_transaction() as session:
+        assert session["user_id"] == initialise_tutor
+
+
+################################# LOGOUT TESTS #################################
+
+def test_logout_no_user(setup_test: FlaskClient, initialise_student: str):
+    client = setup_test
+    with client.session_transaction() as session:
+        assert ("user_id" not in session) == True
+    resp = client.post("/logout", json = {})
+    assert resp.status_code == 400
+    assert resp.json == {"error": "Invalid login attempt"}
+
+def test_logout_student(setup_test: FlaskClient, initialise_student: str):
+    client = setup_test
+    client.post(
+        "/login",
+        json={
+            "email": "validemail@mail.com",
+            "password": "12345678",
+            "accountType": "student",
+        },
+    )
+
+    with client.session_transaction() as session:
+        assert session["user_id"] == initialise_student
+
+    resp = client.post("/logout", json = {})
+    assert resp.status_code == 200
+    assert resp.json["success"] == True
+
+def test_logout_tutor(setup_test: FlaskClient, initialise_tutor: str):
+    client = setup_test
+    client.post(
+        "/login",
+        json={
+            "email": "validemail2@mail.com",
+            "password": "12345678",
+            "accountType": "tutor",
+        },
+    )
+
+    with client.session_transaction() as session:
+        assert session["user_id"] == initialise_tutor
+
+    resp = client.post("/logout", json = {})
+    assert resp.status_code == 200
+    assert resp.json["success"] == True
