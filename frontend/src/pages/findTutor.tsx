93443b04e18224f9c6ece9df715880d64a16e3dd
ddannyll <daniel.nguyen.s173@gmail.com>
2023-10-09 02:00:22 +1100

    save work cause im going to bed

--- frontend/src/pages/findTutor.tsx
+++ frontend/src/pages/findTutor.tsx
@@ -1,6 +1,8 @@
 import SmallProfileCard from "@/components/smallProfileCard"
+import { useQuery } from "react-query"
 
 export default function FindTutor() {
+  const { data: tutorIds } = useQuery({})
   return (
     <div className="h-full w-full p-16">
       <SmallProfileCard id="1" accountType="tutor" />
