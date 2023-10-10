43bd8e1657cf1012c13620545a4228a8fd5248a1
K0FFE1NE <danielwang24680@gmail.com>
2023-10-10 21:25:21 +1100

    updated views to accept email search

--- backend/helpers/views.py
+++ backend/helpers/views.py
@@ -1,21 +1,36 @@
 from prisma.models import User
 
 
-def user(id: str) -> User | None:
-    user = User.prisma().find_unique(where={"id", id})
+def user_view(id: str = None, email: str = None) -> User | None:
+    if id == None and email == None:
+        return None
+
+    search_by = {"id": id} if id else {"email": email}
+    user = User.prisma().find_unique(where=search_by)
+
     return user if user != None else None
 
 
-def student(id: str) -> User | None:
+def student_view(id: str = None, email: str = None) -> User | None:
+    if id == None and email == None:
+        return None
+
+    search_by = {"id": id} if id else {"email": email}
     user = User.prisma().find_unique(
-        where={"id": id}, include={"studentInfo": {"include": {"appointments": True}}}
+        where=search_by,
+        include={"studentInfo": {"include": {"appointments": True}}},
     )
+
     return user if user != None and user.studentInfo != None else None
 
 
-def tutor(id: str) -> User | None:
+def tutor_view(id: str = None, email: str = None) -> User | None:
+    if id == None and email == None:
+        return None
+
+    search_by = {"id": id} if id else {"email": email}
     user = User.prisma().find_unique(
-        where={"id": id},
+        where=search_by,
         include={
             "tutorInfo": {
                 "include": {
@@ -29,9 +44,13 @@ def tutor(id: str) -> User | None:
     return user if user != None and user.tutorInfo != None else None
 
 
-def admin(id: str) -> User | None:
+def admin_view(id: str = None, email: str = None) -> User | None:
+    if id == None and email == None:
+        return None
+
+    search_by = {"id": id} if id else {"email": email}
     user = User.prisma().find_unique(
-        where={"id": id},
+        where=search_by,
         include={"adminInfo": True},
     )
     return user if user != None and user.adminInfo != None else None
