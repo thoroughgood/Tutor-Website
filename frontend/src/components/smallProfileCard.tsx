3b2b8b2bc42c82588635f1af15475627580a5802
ddannyll <daniel.nguyen.s173@gmail.com>
2023-10-17 01:17:54 +1100

    FV39000-91 name + location search

--- frontend/src/components/smallProfileCard.tsx
+++ frontend/src/components/smallProfileCard.tsx
@@ -1,70 +1,87 @@
 import { useQuery } from "react-query"
-import { MockProfileService } from "@/service/profileService"
+import { HTTPProfileService } from "@/service/profileService"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Badge } from "./ui/badge"
 import { MapPin } from "lucide-react"
 import HeaderSeparator from "./headerSeparator"
 import Link from "next/link"
+import { Skeleton } from "./ui/skeleton"
 
 interface SmallProfileCard {
   id: string
   accountType: "tutor" // add more later if necessary
 }
-const profileService = new MockProfileService()
+const profileService = new HTTPProfileService()
 export default function SmallProfileCard({
   id,
   accountType,
 }: SmallProfileCard) {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: [`${accountType}s`, id],
     queryFn: async () => {
       return await profileService.getTutorProfile(id)
     },
   })
-
-  if (!data) {
-    return <div>loading</div>
-  }
   return (
     <Link
       href={`/tutor/${id}`}
       className="flex w-screen max-w-md gap-5 rounded-md border p-5 shadow transition hover:scale-[102%]"
     >
-      <Avatar className="h-32 w-32 border-2 text-4xl ">
-        {data.profilePicture && <AvatarImage src={data.profilePicture} />}
-        <AvatarFallback>
-          {data.name
-            .split(" ")
-            .map((n) => n[0])
-            .join("")}
-        </AvatarFallback>
+      <Avatar className="h-32 w-32 text-4xl ">
+        {data ? (
+          <>
+            {data.profilePicture && <AvatarImage src={data.profilePicture} />}
+            <AvatarFallback>
+              {data.name
+                .split(" ")
+                .map((n) => n[0])
+                .join("")}
+            </AvatarFallback>
+          </>
+        ) : (
+          <Skeleton className="h-full w-full" />
+        )}
       </Avatar>
       <div className="flex grow flex-col justify-between">
-        <div className="flex flex-col items-center gap-2">
-          <h2 className="text-2xl font-semibold text-foreground">
-            {data.name}
-          </h2>
-          <div className="flex items-center gap-4">
-            <Badge
-              variant="outline"
-              className="mt-1 border-muted-foreground px-3 py-1 text-xs text-muted-foreground"
-            >
-              TUTOR
-            </Badge>
-            <div className="flex gap-1 text-muted-foreground">
-              <MapPin className="w-6" />
-              {data.location}
+        {data ? (
+          <>
+            <div className="flex flex-col items-center gap-2">
+              <h2 className="w-full text-center text-2xl font-semibold text-foreground">
+                {data.name}
+              </h2>
+              <div className="flex items-center gap-4">
+                <Badge
+                  variant="outline"
+                  className="mt-1 border-muted-foreground px-3 py-1 text-xs text-muted-foreground"
+                >
+                  TUTOR
+                </Badge>
+                {data.location && (
+                  <div className="flex gap-1 text-muted-foreground">
+                    <MapPin className="w-6" />
+                    {data.location}
+                  </div>
+                )}
+              </div>
+            </div>
+            <HeaderSeparator>Course Offerings</HeaderSeparator>
+            <div className=" flex flex-wrap justify-center gap-2">
+              {data.courseOfferings.map((course) => (
+                <Badge variant="random" key={course}>
+                  {course}
+                </Badge>
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className="space-y-2">
+            <Skeleton className="h-10" />
+            <div className="flex w-full gap-2">
+              <Skeleton className="h-8 w-full" />
+              <Skeleton className="h-8 w-full" />
             </div>
           </div>
-        </div>
-        <HeaderSeparator>Course Offerings</HeaderSeparator>
-        <div className=" flex flex-wrap justify-center gap-2">
-          {data.courseOfferings.map((course) => (
-            <Badge variant="random" key={course}>
-              {course}
-            </Badge>
-          ))}
-        </div>
+        )}
       </div>
     </Link>
   )
--- frontend/src/pages/findTutor.tsx
+++ frontend/src/pages/findTutor.tsx
@@ -1,11 +1,73 @@
+import IconInput from "@/components/iconInput"
 import SmallProfileCard from "@/components/smallProfileCard"
+import { HTTPProfileService } from "@/service/profileService"
+import { Loader2, MapPin, User } from "lucide-react"
+import { useMemo, useState } from "react"
 import { useQuery } from "react-query"
+import { useDebounce } from "usehooks-ts"
 
+const profileService = new HTTPProfileService()
 export default function FindTutor() {
-  const { data: tutorIds } = useQuery({})
+  const [name, setName] = useState<string>("")
+  const [location, setLocation] = useState<string>("")
+  const [rating, setRating] = useState<null | number>(null)
+  const [courseOfferings, setCourseOfferings] = useState<null | string[]>(null)
+  const [startTime, setStartTime] = useState<null | Date>(null)
+  const [endTime, setEndTime] = useState<null | Date>(null)
+
+  const searchParams = useMemo(() => {
+    // Build the param obj from all the fields
+    // Deletes key if 'null' since we want to use whitelist filtering
+    const allParams = {
+      name: name === "" ? null : name,
+      location: location === "" ? null : location,
+      rating,
+      courseOfferings,
+      timeRange: {
+        startTime,
+        endTime,
+      },
+    }
+    return Object.fromEntries(
+      Object.entries(allParams).filter(
+        ([k, v]) =>
+          v !== null &&
+          !(k === "timeRange" && (startTime === null || endTime === null)),
+      ),
+    )
+  }, [name, location, rating, courseOfferings, startTime, endTime])
+
+  const debouncedSearchParams = useDebounce(searchParams, 250)
+
+  const { data: searchResp, isLoading } = useQuery({
+    queryKey: ["tutors", debouncedSearchParams],
+    queryFn: async () => {
+      return await profileService.searchTutors(debouncedSearchParams)
+    },
+  })
   return (
     <div className="h-full w-full p-16">
-      <SmallProfileCard id="1" accountType="tutor" />
+      <div className="flex w-full gap-2">
+        <IconInput
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Name"
+        >
+          <User />
+        </IconInput>
+        <IconInput
+          onChange={(e) => setLocation(e.currentTarget.value)}
+          placeholder="Location"
+        >
+          <MapPin />
+        </IconInput>
+      </div>
+      {isLoading ? (
+        <Loader2 className="animate-spin" />
+      ) : (
+        searchResp?.tutorIds.map((tId) => (
+          <SmallProfileCard key={tId} id={tId} accountType="tutor" />
+        ))
+      )}
     </div>
   )
 }
