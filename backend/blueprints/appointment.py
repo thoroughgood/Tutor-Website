7e0b361ed1fd23176a8cb13e73d972d49d3493ff
Victor Tang <ultrodude@gmail.com>
2023-10-17 16:14:32 +1100

    FV3900-175 created a draft implementation of rating

--- backend/blueprints/appointment.py
+++ backend/blueprints/appointment.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, session
-from prisma.models import User, Appointment
+from prisma.models import User, Appointment, Rating
+from uuid import uuid4
 from datetime import datetime
 from helpers.views import student_view, tutor_view
 from helpers.admin_id_check import admin_id_check
@@ -25,8 +26,8 @@ def request():
     if "endTime" not in args or len(str(args["endTime"]).lower().strip()) == 0:
         raise ExpectedError("endTime field was missing", 400)
 
-    st = datetime.fromisoformat(timeblock["startTime"])
-    et = datetime.fromisoformat(timeblock["endTime"])
+    st = datetime.fromisoformat(args["startTime"])
+    et = datetime.fromisoformat(args["endTime"])
 
     if (et - st).total_seconds() <= 0:
         raise ExpectedError("endTime is before startTime", 400)
@@ -42,7 +43,7 @@ def request():
 
     appointment = Appointment.prisma().create(
         data={
-            "id": str(uuid.uuid4()),
+            "id": str(uuid4()),
             "startTime": args["startTime"],
             "endTime": args["endTime"],
             "studentId": session["user_id"],
@@ -51,7 +52,7 @@ def request():
         }
     )
 
-    Student.prisma().update(
+    User.prisma().update(
         where={"id": session["user_id"]},
         data={"appointments": student.appointments + [appointment]},
     )
@@ -69,3 +70,33 @@ def request():
         ),
         200,
     )
+
+
+@appointment.route("rating/", methods=["POST"])
+@error_decorator
+def rating():
+    args = request.get_json()
+
+    if "user_id" not in session:
+        raise ExpectedError("No user is logged in", 401)
+
+    if "id" not in args or len(str(args["id"]).lower().strip()) == 0:
+        raise ExpectedError("id field was missing", 400)
+
+    if "rating" not in args or len(str(args["rating"]).lower().strip()) == 0:
+        raise ExpectedError("rating field was missing", 400)
+
+    appointment = Appointment.prisma().find_unique(where={"id": args["id"]})
+    if not appointment:
+        raise ExpectedError("Appointment does not exist", 400)
+
+    if not 1 <= args["rating"] <= 5:
+        raise ExpectedError("Rating must be between 1 and 5", 400)
+
+    if appointment.endTime > datetime.now():
+        raise ExpectedError("Appointment isn't complete yet", 400)
+
+    if session["user_id"] != appointment.studentId:
+        raise ExpectedError("User is not the student of the appointment", 400)
+
+    rating = Rating.prisma().create()
