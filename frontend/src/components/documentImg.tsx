ba1bec13fd987cb92a15ddcd3967319177da0199
Terry Thoroughgood <thoroughgoodterry@gmail.com>
2023-11-11 23:30:19 +1100

    fixed small visual issue

--- frontend/src/components/documentImg.tsx
+++ frontend/src/components/documentImg.tsx
@@ -43,7 +43,7 @@ export default function DocumentImg({ documentId }: documentImgInterface) {
       return <></>
     }
     return (
-      <div className="mt-5 flex h-1/2 w-5/6 flex-grow">
+      <div className="mt-5 flex h-1/2 w-5/6">
         <iframe
           src={data.document}
           className="h-full w-full rounded border-2 border-inherit"
