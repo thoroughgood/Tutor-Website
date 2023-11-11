5d7fce421e4fb3e64090f88f9d05b8031341fecf
K0FFE1NE <danielwang24680@gmail.com>
2023-11-11 21:43:46 +1100

    Configured gunicorn for prod + modified status codes in utils

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,16 +1,19 @@
+from multiprocessing import cpu_count
+
 # config file for gunicorn
 # see https://docs.gunicorn.org/en/stable/settings.html for more
 
 wsgi_app = "wsgi:app"
-workers = 4
+workers = (2 * cpu_count()) + 1
 threads = 2
 worker_class = "gthread"
+loglevel = "critical"
 
 # https://docs.gunicorn.org/en/stable/settings.html#daemon
 # daemon = True
 
 # debug options
 # https://docs.gunicorn.org/en/stable/settings.html#loglevel
-loglevel = "debug"
+# loglevel = "debug"
 # https://docs.gunicorn.org/en/stable/settings.html#reload
-reload = True
+# reload = True
