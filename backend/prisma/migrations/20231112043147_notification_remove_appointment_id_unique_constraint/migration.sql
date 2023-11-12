33e8ed89bb8f2a6e4ecf9f7260e9cb6b73b133bc
K0FFE1NE <danielwang24680@gmail.com>
2023-11-12 15:32:25 +1100

    Removed unique constraint on appointmentId for notification model

---
+++ backend/prisma/migrations/20231112043147_notification_remove_appointment_id_unique_constraint/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "Notification_appointmentId_key";
+
+-- DropIndex
+DROP INDEX "Notification_appointmentId_userId_key";
+
+-- DropIndex
+DROP INDEX "Notification_messageId_userId_key";
